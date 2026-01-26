5be4f6bd3d96f9b7d07ab1eec95cd201767f7af8
MayaOfLagos <mayaoflagos@gmail.com>
2026-01-26 13:30:01 +0100

    style: Larger icons in Core Values section with mobile optimization

---
+++ public/build/assets/index-Cm1bONHA.js
@@ -0,0 +1,126 @@
+import{c,f as y,b as $}from"./index-p8MMPEpC.js";import{ad as h,B as m,A as l,c as u,o as a,M as s,a as v,ap as g,aq as b,g as x,b as C,ar as F}from"./app-BXhd72r4.js";var I={name:"BaseEditableHolder",extends:c,emits:["update:modelValue","value-change"],props:{modelValue:{type:null,default:void 0},defaultValue:{type:null,default:void 0},name:{type:String,default:void 0},invalid:{type:Boolean,default:void 0},disabled:{type:Boolean,default:!1},formControl:{type:Object,default:void 0}},inject:{$parentInstance:{default:void 0},$pcForm:{default:void 0},$pcFormField:{default:void 0}},data:function(){return{d_value:this.defaultValue!==void 0?this.defaultValue:this.modelValue}},watch:{modelValue:{deep:!0,handler:function(n){this.d_value=n}},defaultValue:function(n){this.d_value=n},$formName:{immediate:!0,handler:function(n){var e,i;this.formField=((e=this.$pcForm)===null||e===void 0||(i=e.register)===null||i===void 0?void 0:i.call(e,n,this.$formControl))||{}}},$formControl:{immediate:!0,handler:function(n){var e,i;this.formField=((e=this.$pcForm)===null||e===void 0||(i=e.register)===null||i===void 0?void 0:i.call(e,this.$formName,n))||{}}},$formDefaultValue:{immediate:!0,handler:function(n){this.d_value!==n&&(this.d_value=n)}},$formValue:{immediate:!1,handler:function(n){var e;(e=this.$pcForm)!==null&&e!==void 0&&e.getFieldState(this.$formName)&&n!==this.d_value&&(this.d_value=n)}}},formField:{},methods:{writeValue:function(n,e){var i,o;this.controlled&&(this.d_value=n,this.$emit("update:modelValue",n)),this.$emit("value-change",n),(i=(o=this.formField).onChange)===null||i===void 0||i.call(o,{originalEvent:e,value:n})},findNonEmpty:function(){for(var n=arguments.length,e=new Array(n),i=0;i<n;i++)e[i]=arguments[i];return e.find(h)}},computed:{$filled:function(){return h(this.d_value)},$invalid:function(){var n,e;return!this.$formNovalidate&&this.findNonEmpty(this.invalid,(n=this.$pcFormField)===null||n===void 0||(n=n.$field)===null||n===void 0?void 0:n.invalid,(e=this.$pcForm)===null||e===void 0||(e=e.getFieldState(this.$formName))===null||e===void 0?void 0:e.invalid)},$formName:function(){var n;return this.$formNovalidate?void 0:this.name||((n=this.$formControl)===null||n===void 0?void 0:n.name)},$formControl:function(){var n;return this.formControl||((n=this.$pcFormField)===null||n===void 0?void 0:n.formControl)},$formNovalidate:function(){var n;return(n=this.$formControl)===null||n===void 0?void 0:n.novalidate},$formDefaultValue:function(){var n,e;return this.findNonEmpty(this.d_value,(n=this.$pcFormField)===null||n===void 0?void 0:n.initialValue,(e=this.$pcForm)===null||e===void 0||(e=e.initialValues)===null||e===void 0?void 0:e[this.$formName])},$formValue:function(){var n,e;return this.findNonEmpty((n=this.$pcFormField)===null||n===void 0||(n=n.$field)===null||n===void 0?void 0:n.value,(e=this.$pcForm)===null||e===void 0||(e=e.getFieldState(this.$formName))===null||e===void 0?void 0:e.value)},controlled:function(){return this.$inProps.hasOwnProperty("modelValue")||!this.$inProps.hasOwnProperty("modelValue")&&!this.$inProps.hasOwnProperty("defaultValue")},filled:function(){return this.$filled}}},w={name:"BaseInput",extends:I,props:{size:{type:String,default:null},fluid:{type:Boolean,default:null},variant:{type:String,default:null}},inject:{$parentInstance:{default:void 0},$pcFluid:{default:void 0}},computed:{$variant:function(){var n;return(n=this.variant)!==null&&n!==void 0?n:this.$primevue.config.inputStyle||this.$primevue.config.inputVariant},$fluid:function(){var n;return(n=this.fluid)!==null&&n!==void 0?n:!!this.$pcFluid},hasFluid:function(){return this.$fluid}}},_=`
+    .p-inputtext {
+        font-family: inherit;
+        font-feature-settings: inherit;
+        font-size: 1rem;
+        color: dt('inputtext.color');
+        background: dt('inputtext.background');
+        padding-block: dt('inputtext.padding.y');
+        padding-inline: dt('inputtext.padding.x');
+        border: 1px solid dt('inputtext.border.color');
+        transition:
+            background dt('inputtext.transition.duration'),
+            color dt('inputtext.transition.duration'),
+            border-color dt('inputtext.transition.duration'),
+            outline-color dt('inputtext.transition.duration'),
+            box-shadow dt('inputtext.transition.duration');
+        appearance: none;
+        border-radius: dt('inputtext.border.radius');
+        outline-color: transparent;
+        box-shadow: dt('inputtext.shadow');
+    }
+
+    .p-inputtext:enabled:hover {
+        border-color: dt('inputtext.hover.border.color');
+    }
+
+    .p-inputtext:enabled:focus {
+        border-color: dt('inputtext.focus.border.color');
+        box-shadow: dt('inputtext.focus.ring.shadow');
+        outline: dt('inputtext.focus.ring.width') dt('inputtext.focus.ring.style') dt('inputtext.focus.ring.color');
+        outline-offset: dt('inputtext.focus.ring.offset');
+    }
+
+    .p-inputtext.p-invalid {
+        border-color: dt('inputtext.invalid.border.color');
+    }
+
+    .p-inputtext.p-variant-filled {
+        background: dt('inputtext.filled.background');
+    }
+
+    .p-inputtext.p-variant-filled:enabled:hover {
+        background: dt('inputtext.filled.hover.background');
+    }
+
+    .p-inputtext.p-variant-filled:enabled:focus {
+        background: dt('inputtext.filled.focus.background');
+    }
+
+    .p-inputtext:disabled {
+        opacity: 1;
+        background: dt('inputtext.disabled.background');
+        color: dt('inputtext.disabled.color');
+    }
+
+    .p-inputtext::placeholder {
+        color: dt('inputtext.placeholder.color');
+    }
+
+    .p-inputtext.p-invalid::placeholder {
+        color: dt('inputtext.invalid.placeholder.color');
+    }
+
+    .p-inputtext-sm {
+        font-size: dt('inputtext.sm.font.size');
+        padding-block: dt('inputtext.sm.padding.y');
+        padding-inline: dt('inputtext.sm.padding.x');
+    }
+
+    .p-inputtext-lg {
+        font-size: dt('inputtext.lg.font.size');
+        padding-block: dt('inputtext.lg.padding.y');
+        padding-inline: dt('inputtext.lg.padding.x');
+    }
+
+    .p-inputtext-fluid {
+        width: 100%;
+    }
+`,S={root:function(n){var e=n.instance,i=n.props;return["p-inputtext p-component",{"p-filled":e.$filled,"p-inputtext-sm p-inputfield-sm":i.size==="small","p-inputtext-lg p-inputfield-lg":i.size==="large","p-invalid":e.$invalid,"p-variant-filled":e.$variant==="filled","p-inputtext-fluid":e.$fluid}]}},A=m.extend({name:"inputtext",style:_,classes:S}),V={name:"BaseInputText",extends:w,style:A,provide:function(){return{$pcInputText:this,$parentInstance:this}}};function d(t){"@babel/helpers - typeof";return d=typeof Symbol=="function"&&typeof Symbol.iterator=="symbol"?function(n){return typeof n}:function(n){return n&&typeof Symbol=="function"&&n.constructor===Symbol&&n!==Symbol.prototype?"symbol":typeof n},d(t)}function z(t,n,e){return(n=k(n))in t?Object.defineProperty(t,n,{value:e,enumerable:!0,configurable:!0,writable:!0}):t[n]=e,t}function k(t){var n=N(t,"string");return d(n)=="symbol"?n:n+""}function N(t,n){if(d(t)!="object"||!t)return t;var e=t[Symbol.toPrimitive];if(e!==void 0){var i=e.call(t,n);if(d(i)!="object")return i;throw new TypeError("@@toPrimitive must return a primitive value.")}return(n==="string"?String:Number)(t)}var B={name:"InputText",extends:V,inheritAttrs:!1,methods:{onInput:function(n){this.writeValue(n.target.value,n)}},computed:{attrs:function(){return l(this.ptmi("root",{context:{filled:this.$filled,disabled:this.disabled}}),this.formField)},dataP:function(){return y(z({invalid:this.$invalid,fluid:this.$fluid,filled:this.$variant==="filled"},this.size,this.size))}}},T=["value","name","disabled","aria-invalid","data-p"];function P(t,n,e,i,o,r){return a(),u("input",l({type:"text",class:t.cx("root"),value:t.d_value,name:t.name,disabled:t.disabled,"aria-invalid":t.$invalid||void 0,"data-p":r.dataP,onInput:n[0]||(n[0]=function(){return r.onInput&&r.onInput.apply(r,arguments)})},r.attrs),null,16,T)}B.render=P;var L=`
+    .p-iconfield {
+        position: relative;
+        display: block;
+    }
+
+    .p-inputicon {
+        position: absolute;
+        top: 50%;
+        margin-top: calc(-1 * (dt('icon.size') / 2));
+        color: dt('iconfield.icon.color');
+        line-height: 1;
+        z-index: 1;
+    }
+
+    .p-iconfield .p-inputicon:first-child {
+        inset-inline-start: dt('form.field.padding.x');
+    }
+
+    .p-iconfield .p-inputicon:last-child {
+        inset-inline-end: dt('form.field.padding.x');
+    }
+
+    .p-iconfield .p-inputtext:not(:first-child),
+    .p-iconfield .p-inputwrapper:not(:first-child) .p-inputtext {
+        padding-inline-start: calc((dt('form.field.padding.x') * 2) + dt('icon.size'));
+    }
+
+    .p-iconfield .p-inputtext:not(:last-child) {
+        padding-inline-end: calc((dt('form.field.padding.x') * 2) + dt('icon.size'));
+    }
+
+    .p-iconfield:has(.p-inputfield-sm) .p-inputicon {
+        font-size: dt('form.field.sm.font.size');
+        width: dt('form.field.sm.font.size');
+        height: dt('form.field.sm.font.size');
+        margin-top: calc(-1 * (dt('form.field.sm.font.size') / 2));
+    }
+
+    .p-iconfield:has(.p-inputfield-lg) .p-inputicon {
+        font-size: dt('form.field.lg.font.size');
+        width: dt('form.field.lg.font.size');
+        height: dt('form.field.lg.font.size');
+        margin-top: calc(-1 * (dt('form.field.lg.font.size') / 2));
+    }
+`,E={root:"p-iconfield"},j=m.extend({name:"iconfield",style:L,classes:E}),O={name:"BaseIconField",extends:c,style:j,provide:function(){return{$pcIconField:this,$parentInstance:this}}},M={name:"IconField",extends:O,inheritAttrs:!1};function D(t,n,e,i,o,r){return a(),u("div",l({class:t.cx("root")},t.ptmi("root")),[s(t.$slots,"default")],16)}M.render=D;var H={root:"p-inputicon"},U=m.extend({name:"inputicon",classes:H}),W={name:"BaseInputIcon",extends:c,style:U,props:{class:null},provide:function(){return{$pcInputIcon:this,$parentInstance:this}}},Z={name:"InputIcon",extends:W,inheritAttrs:!1,computed:{containerClass:function(){return[this.cx("root"),this.class]}}};function q(t,n,e,i,o,r){return a(),u("span",l({class:r.containerClass},t.ptmi("root"),{"aria-hidden":"true"}),[s(t.$slots,"default")],16)}Z.render=q;var K={name:"CheckIcon",extends:$};function G(t){return X(t)||R(t)||Q(t)||J()}function J(){throw new TypeError(`Invalid attempt to spread non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}function Q(t,n){if(t){if(typeof t=="string")return p(t,n);var e={}.toString.call(t).slice(8,-1);return e==="Object"&&t.constructor&&(e=t.constructor.name),e==="Map"||e==="Set"?Array.from(t):e==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(e)?p(t,n):void 0}}function R(t){if(typeof Symbol<"u"&&t[Symbol.iterator]!=null||t["@@iterator"]!=null)return Array.from(t)}function X(t){if(Array.isArray(t))return p(t)}function p(t,n){(n==null||n>t.length)&&(n=t.length);for(var e=0,i=Array(n);e<n;e++)i[e]=t[e];return i}function Y(t,n,e,i,o,r){return a(),u("svg",l({width:"14",height:"14",viewBox:"0 0 14 14",fill:"none",xmlns:"http://www.w3.org/2000/svg"},t.pti()),G(n[0]||(n[0]=[v("path",{d:"M4.86199 11.5948C4.78717 11.5923 4.71366 11.5745 4.64596 11.5426C4.57826 11.5107 4.51779 11.4652 4.46827 11.4091L0.753985 7.69483C0.683167 7.64891 0.623706 7.58751 0.580092 7.51525C0.536478 7.44299 0.509851 7.36177 0.502221 7.27771C0.49459 7.19366 0.506156 7.10897 0.536046 7.03004C0.565935 6.95111 0.613367 6.88 0.674759 6.82208C0.736151 6.76416 0.8099 6.72095 0.890436 6.69571C0.970973 6.67046 1.05619 6.66385 1.13966 6.67635C1.22313 6.68886 1.30266 6.72017 1.37226 6.76792C1.44186 6.81567 1.4997 6.8786 1.54141 6.95197L4.86199 10.2503L12.6397 2.49483C12.7444 2.42694 12.8689 2.39617 12.9932 2.40745C13.1174 2.41873 13.2343 2.47141 13.3251 2.55705C13.4159 2.64268 13.4753 2.75632 13.4938 2.87973C13.5123 3.00315 13.4888 3.1292 13.4271 3.23768L5.2557 11.4091C5.20618 11.4652 5.14571 11.5107 5.07801 11.5426C5.01031 11.5745 4.9368 11.5923 4.86199 11.5948Z",fill:"currentColor"},null,-1)])),16)}K.render=Y;var tt={name:"TimesIcon",extends:$};function nt(t){return ot(t)||rt(t)||it(t)||et()}function et(){throw new TypeError(`Invalid attempt to spread non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}function it(t,n){if(t){if(typeof t=="string")return f(t,n);var e={}.toString.call(t).slice(8,-1);return e==="Object"&&t.constructor&&(e=t.constructor.name),e==="Map"||e==="Set"?Array.from(t):e==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(e)?f(t,n):void 0}}function rt(t){if(typeof Symbol<"u"&&t[Symbol.iterator]!=null||t["@@iterator"]!=null)return Array.from(t)}function ot(t){if(Array.isArray(t))return f(t)}function f(t,n){(n==null||n>t.length)&&(n=t.length);for(var e=0,i=Array(n);e<n;e++)i[e]=t[e];return i}function lt(t,n,e,i,o,r){return a(),u("svg",l({width:"14",height:"14",viewBox:"0 0 14 14",fill:"none",xmlns:"http://www.w3.org/2000/svg"},t.pti()),nt(n[0]||(n[0]=[v("path",{d:"M8.01186 7.00933L12.27 2.75116C12.341 2.68501 12.398 2.60524 12.4375 2.51661C12.4769 2.42798 12.4982 2.3323 12.4999 2.23529C12.5016 2.13827 12.4838 2.0419 12.4474 1.95194C12.4111 1.86197 12.357 1.78024 12.2884 1.71163C12.2198 1.64302 12.138 1.58893 12.0481 1.55259C11.9581 1.51625 11.8617 1.4984 11.7647 1.50011C11.6677 1.50182 11.572 1.52306 11.4834 1.56255C11.3948 1.60204 11.315 1.65898 11.2488 1.72997L6.99067 5.98814L2.7325 1.72997C2.59553 1.60234 2.41437 1.53286 2.22718 1.53616C2.03999 1.53946 1.8614 1.61529 1.72901 1.74767C1.59663 1.88006 1.5208 2.05865 1.5175 2.24584C1.5142 2.43303 1.58368 2.61419 1.71131 2.75116L5.96948 7.00933L1.71131 11.2675C1.576 11.403 1.5 11.5866 1.5 11.7781C1.5 11.9696 1.576 12.1532 1.71131 12.2887C1.84679 12.424 2.03043 12.5 2.2219 12.5C2.41338 12.5 2.59702 12.424 2.7325 12.2887L6.99067 8.03052L11.2488 12.2887C11.3843 12.424 11.568 12.5 11.7594 12.5C11.9509 12.5 12.1346 12.424 12.27 12.2887C12.4053 12.1532 12.4813 11.9696 12.4813 11.7781C12.4813 11.5866 12.4053 11.403 12.27 11.2675L8.01186 7.00933Z",fill:"currentColor"},null,-1)])),16)}tt.render=lt;var pt=g(),at={name:"Portal",props:{appendTo:{type:[String,Object],default:"body"},disabled:{type:Boolean,default:!1}},data:function(){return{mounted:!1}},mounted:function(){this.mounted=b()},computed:{inline:function(){return this.disabled||this.appendTo==="self"}}};function dt(t,n,e,i,o,r){return r.inline?s(t.$slots,"default",{key:0}):o.mounted?(a(),x(F,{key:1,to:e.appendTo},[s(t.$slots,"default")],8,["to"])):C("",!0)}at.render=dt;export{pt as O,M as a,Z as b,w as c,tt as d,at as e,K as f,B as s};
--- resources/js/Components/Landing/Sections/CoreValuesSection.vue
+++ resources/js/Components/Landing/Sections/CoreValuesSection.vue
@@ -58,7 +58,7 @@ onMounted(() => {
             </div>
 
             <!-- Values Grid -->
-            <div class="grid md:grid-cols-3 gap-8 lg:gap-12">
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-10 lg:gap-12">
                 <div 
                     v-for="(value, index) in values"
                     :key="value.title"
@@ -67,17 +67,17 @@ onMounted(() => {
                     :style="{ animationDelay: `${(index + 1) * 0.15}s` }"
                 >
                     <!-- Icon -->
-                    <div class="w-16 h-16 mx-auto mb-6 bg-white rounded-2xl shadow-sm flex items-center justify-center border border-gray-100">
+                    <div class="w-20 h-20 sm:w-18 sm:h-18 lg:w-20 lg:h-20 mx-auto mb-6 bg-white rounded-2xl shadow-sm flex items-center justify-center border border-gray-100">
                         <!-- Shield Icon -->
-                        <svg v-if="value.icon === 'shield'" class="w-8 h-8 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg v-if="value.icon === 'shield'" class="w-10 h-10 sm:w-9 sm:h-9 lg:w-10 lg:h-10 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                         </svg>
                         <!-- Lock Icon -->
-                        <svg v-else-if="value.icon === 'lock'" class="w-8 h-8 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg v-else-if="value.icon === 'lock'" class="w-10 h-10 sm:w-9 sm:h-9 lg:w-10 lg:h-10 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                         </svg>
                         <!-- Sparkle Icon -->
-                        <svg v-else class="w-8 h-8 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg v-else class="w-10 h-10 sm:w-9 sm:h-9 lg:w-10 lg:h-10 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
                         </svg>
                     </div>
