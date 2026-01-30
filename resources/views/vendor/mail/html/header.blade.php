a5e11c8d468b41be8f13b97be7597c9537c66949
MayaOfLagos <mayaoflagos@gmail.com>
2026-01-30 19:15:45 +0100

    fix: Use PNG logo for emails instead of SVG (Gmail compatibility)
    Gmail doesn't support SVG images in emails, causing broken logo display.
    Updated email header to:
    1. Check for dedicated email_logo setting first
    2. If site_logo is SVG, fallback to site_favicon (PNG)
    3. This ensures Gmail and other restrictive clients display the logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,18 @@
 @props(['url'])
 @php
-    $logoPath = \App\Models\Setting::getValue('branding', 'site_logo');
+    // Use email_logo (PNG) for emails, fallback to site_logo
+    // Gmail doesn't support SVG images, so we need PNG for email
+    $logoPath = \App\Models\Setting::getValue('branding', 'email_logo')
+        ?? \App\Models\Setting::getValue('branding', 'site_logo');
+
+    // If the logo is SVG, try to use favicon as fallback (usually PNG)
+    if ($logoPath && str_ends_with(strtolower($logoPath), '.svg')) {
+        $faviconPath = \App\Models\Setting::getValue('branding', 'site_favicon');
+        if ($faviconPath && !str_ends_with(strtolower($faviconPath), '.svg')) {
+            $logoPath = $faviconPath;
+        }
+    }
+
     $logoUrl = $logoPath ? url(\Illuminate\Support\Facades\Storage::url($logoPath)) : null;
     $appName = app_name();
 @endphp
